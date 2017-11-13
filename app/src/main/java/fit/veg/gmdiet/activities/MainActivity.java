d15a8306aa041776b599d26d7b570b73b4165a19
Brijesh Masrani <brijesh.masrani@byjus.com>
2017-11-13 17:36:33 +0530

    initial project setup

---
+++ app/src/main/java/fit/veg/gmdiet/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package fit.veg.gmdiet.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import fit.veg.gmdiet.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
